d0ff83dc52ddc6daba41330065e639d02ab7e8a9
CS23students <153345000+CS23students@users.noreply.github.com>
2024-08-19 21:45:42 +0530

    Create 1-c-circular-queue.c

---
+++ 1-c-circular-queue.c
@@ -0,0 +1,73 @@
+// circular queue
+
+#include <stdio.h>
+
+#define MAX 100 // Maximum size of the queue
+
+int queue[MAX]; // Array to hold queue elements
+int front = 0;  // Index of the front element
+int rear = -1;  // Index of the rear element
+int size = 0;   // Number of elements in the queue
+
+// Enqueue an item
+void enqueue(int value) {
+    if (size < MAX) {
+        rear = (rear + 1) % MAX;
+        queue[rear] = value;
+        size++;
+    } else {
+        printf("Queue overflow\n");
+    }
+}
+
+// Dequeue an item
+int dequeue() {
+    if (size > 0) {
+        int value = queue[front];
+        front = (front + 1) % MAX;
+        size--;
+        return value;
+    } else {
+        printf("Queue underflow\n");
+        return -1; // Error code
+    }
+}
+
+// Peek at the front item
+int peek() {
+    if (size > 0) {
+        return queue[front];
+    } else {
+        printf("Queue is empty\n");
+        return -1; // Error code
+    }
+}
+
+int main() {
+    int value;
+    int choice;
+    
+    while (1) {
+        printf("1. Enqueue\n2. Dequeue\n3. Peek\n4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        
+        switch (choice) {
+            case 1:
+                printf("Enter value to enqueue: ");
+                scanf("%d", &value);
+                enqueue(value);
+                break;
+            case 2:
+                printf("Dequeued item: %d\n", dequeue());
+                break;
+            case 3:
+                printf("Front item: %d\n", peek());
+                break;
+            case 4:
+                return 0;
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+}
